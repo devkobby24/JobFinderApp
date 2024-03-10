7f4c3503b94a4d3a40e51039706384b3487f6e64
devkobby24 <nanakobby2002@gmail.com>
2024-03-10 19:54:17 +0000

    made some updates

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,12 @@
-module.exports = function (api) {
+export default function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
     plugins: [
       "@babel/plugin-proposal-export-namespace-from",
       "react-native-reanimated/plugin",
+      // eslint-disable-next-line no-undef
       require.resolve("expo-router/babel"),
     ],
   };
-};
+}
